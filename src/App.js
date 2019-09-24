d72272b9899394cda67ec273b0cd5a4749868acd
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-24 19:01:04 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import App1 from "./Components/ToggleMenu"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h3>hello world</h3>
+     <App1/>
+    </div>
+  );
+}
+
+export default App;
