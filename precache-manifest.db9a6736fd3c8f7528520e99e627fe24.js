9cf7e0fe16059fffc8bc0d137f1c0cc482fcbc38
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-24 19:03:07 +0300

    Updates

---
+++ precache-manifest.db9a6736fd3c8f7528520e99e627fe24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87e0954ec6bad2d7d08fd06122aa85f",
+    "url": "/hook-example1/index.html"
+  },
+  {
+    "revision": "887fa6cd012a3e60d783",
+    "url": "/hook-example1/static/css/main.c8aecd1a.chunk.css"
+  },
+  {
+    "revision": "f41e4fc57e3c3b92bc47",
+    "url": "/hook-example1/static/js/2.74da8262.chunk.js"
+  },
+  {
+    "revision": "887fa6cd012a3e60d783",
+    "url": "/hook-example1/static/js/main.1bfb31fd.chunk.js"
+  },
+  {
+    "revision": "f34b9b415128354f54ca",
+    "url": "/hook-example1/static/js/runtime-main.86fe148b.js"
+  }
+]);
